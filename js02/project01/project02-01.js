58b2ac399ca38e43552b48e3aca4d612dad6535f
Ola131 <160334841+Ola131@users.noreply.github.com>
2024-10-21 18:41:59 -0400

    Add files via upload

---
+++ js02/project01/project02-01.js
@@ -0,0 +1,11 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-01
+
+      Celsius <-> Farenheit Coverter
+      Author: Olawale Mustapha
+      Date:   10/07/2024
+
+      Filename: project02-01.js
+ */
+
